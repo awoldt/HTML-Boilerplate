29eb7079fdc5af93712785b5a606b1f5c23b11ae
unknown <abreckwoldt23@gmail.com>
2022-06-27 10:04:37 -0700

    stable

--- src/app.ts
+++ src/app.ts
@@ -12,8 +12,11 @@ app.use(
   express.static(path.join(__dirname, "..", "/public"))
 );
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "..", "/index.html"));
+  res.status(200).sendFile(path.join(__dirname, "..", "/index.html"));
 });
+app.get('/privacy', (req, res) => {
+  res.status(200).sendFile(path.join(__dirname, "..", "/privacy.html"));
+})
 
 app.listen(8080, () => {
   console.log(
